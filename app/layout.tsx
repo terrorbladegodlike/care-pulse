7a02cb7e416154b619fb135f07ccb8347c106758
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-08-27 15:43:53 +0300

    Setup App
    Added CSS styles, TailwindCSS styles, also utils.ts, which exports the cn function that merges CSS classes using two utilities: clsx and tailwind-merge.

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from 'next'
+
+// Import Fonts from Next-Font
+import { Plus_Jakarta_Sans } from 'next/font/google'
+
+// Import CSS styles file
+import './globals.css'
+import { cn } from '@/lib/utils'
+
+// Import Dark Mode from Shadcn-ui
+import { ThemeProvider } from '@/components/theme-provider'
+
+const fontSans = Plus_Jakarta_Sans({
+   subsets: ['latin'],
+   weight: ['300', '400', '500', '600', '700'],
+   variable: '--font-sans',
+})
+
+export const metadata: Metadata = {
+   title: 'CarePulse',
+   description: 'A healthcare management service',
+}
+
+export default function RootLayout({
+   children,
+}: Readonly<{
+   children: React.ReactNode
+}>) {
+   return (
+      <html lang='en'>
+         <body
+            className={cn(
+               'min-h-screen bg-dark-300 font-sans antialiased',
+               fontSans.variable
+            )}
+         >
+            <ThemeProvider attribute='class' defaultTheme='dark'>
+               {children}
+            </ThemeProvider>
+         </body>
+      </html>
+   )
+}
